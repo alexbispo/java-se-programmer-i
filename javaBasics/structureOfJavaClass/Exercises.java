c54c576d21df95ed0650e01b2ab244ae717df35b
alexbispo <alexbispo.artes@gmail.com>
2019-07-27 04:51:17 -0300

    Exercise for Structure of Java Class session.

---
+++ javaBasics/structureOfJavaClass/Exercises.java
@@ -0,0 +1,20 @@
+// package a.b.c;
+
+import java.util.*;
+
+class J {}
+
+// import java.io.*; Compile error
+
+class K {}
+
+class Test {
+    int Test = 305;
+    void Test() {
+        System.out.println(Test);
+    }
+
+    public static void main(String[] args) {
+        new Test();
+    }
+}
