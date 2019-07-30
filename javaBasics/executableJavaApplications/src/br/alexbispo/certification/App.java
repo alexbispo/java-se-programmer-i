72fe67435a338ab66b72aba0622b551700ca0b4b
alexbispo <alexbispo.artes@gmail.com>
2019-07-30 05:48:28 -0300

    Learning about classpath and jar files.

--- javaBasics/executableJavaApplications/src/br/alexbispo/certification/App.java
+++ javaBasics/executableJavaApplications/src/br/alexbispo/certification/App.java
@@ -1,11 +1,16 @@
 package br.alexbispo.certification;
 
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
 public class App {
     public static void main(String ...args) {
+        // java -cp classes;lib\commons-lang3-3.9.jar br.alexbispo.certification.App
+
         String out = "...";
 
         if (args.length > 0) out = args[0];
 
+        System.out.println(isEmpty(out));
         System.out.println(out);
     }
 }
