0252ee9c4da91873561ac2c795f7172584c8388f
alexbispo <alexbispo.artes@gmail.com>
2019-07-31 05:20:58 -0300

    More some cases of invalid mehod main.

--- javaBasics/executableJavaApplications/ExecutableClass.java
+++ javaBasics/executableJavaApplications/ExecutableClass.java
@@ -3,6 +3,10 @@ class main {
 
     // static void main(String ...args) {} // invalid method main
 
+    // protected static void main(String[] args) {} // invalid method main
+
+    // private static void main(String[] args) {} // invalid method main
+
     // public static String main(String ...args) { return "Hello World!"; } // invalid method main
 
    public static void main(String args) { System.out.println("Hello " + args + "!"); } // invalid method main
@@ -11,9 +15,10 @@ class main {
 
     // public static void main(String args[]) {} // valid!
 
-   public static void main(String ...argsNameDoesNotMatter) {
+   public static void main(String... argsNameDoesNotMatter) {
         String name = argsNameDoesNotMatter.length > 0 ? argsNameDoesNotMatter[0] : "World";
 
+        System.out.println(argsNameDoesNotMatter[0]);
         main(name);
     }
 }
